3054a890f1de1af3c33ae9b98b29dc094001c67d
Ryo-2525 <seama.24.music@gmail.com>
2021-03-04 03:27:58 +0000

    [start]upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,32 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites, dependent: :destroy
   has_many :book_comments ,dependent: :destroy
+  # フォロー取得
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # フォロワー取得
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 自分がフォローしている人
+  has_many :following_user, through: :follower, source: :followed
+  # 自分をフォローしてる人
+  has_many :follower_user, through: :followed, source: :follower
   
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
+  
+   # ユーザーをフォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+  
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+  following_user.include?(user)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
+  # フォローする
+  post 'follow/:id' => 'relationships#follow', as: 'follow'
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
+
   resources :users,only: [:show,:index,:edit,:update]
 
   resources :books do
